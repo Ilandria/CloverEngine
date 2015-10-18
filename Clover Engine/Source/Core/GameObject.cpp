1a01a97170ef0d087a6d604013f7043a30703f46
Charlotte Brown <charlotte.clover.brown@gmail.com>
2015-10-18 17:58:39 -0400

    - Added a bunch of component getters to CGameObject.

--- Clover Engine/Source/Core/GameObject.cpp
+++ Clover Engine/Source/Core/GameObject.cpp
@@ -11,6 +11,7 @@ Website:	http://charlottebrown.ca/
 
 #include "CloverEngine.h"
 
+using namespace std;
 using namespace CloverEngine;
 
 CGameObject::CGameObject() : m_Transform(nullptr)
@@ -38,4 +39,103 @@ CGameObject::~CGameObject()
 CTransform* CGameObject::GetTransform()
 {
 	return m_Transform;
+}
+
+CComponent* CGameObject::AddComponent(CComponent* aComponent)
+{
+	// Is the given component a null pointer?
+	if (aComponent != nullptr)
+	{
+		// Is the given component valid?
+		if (!aComponent->IsType(CE_ComponentType_Invalid) && !aComponent->IsType(CE_ComponentType_Transform))
+		{
+			// Add the component.
+			m_Components.push_back(aComponent);
+
+			// Return the component (for chaining purposes).
+			return aComponent;
+		}
+	}
+
+	// Not valid - return a null pointer.
+	return nullptr;
+}
+
+CComponent* CGameObject::GetComponent(const int& aType)
+{
+	// Search through the components.
+	for (auto component : m_Components)
+	{
+		// If we found the correct one...
+		if (component->IsType(aType))
+		{
+			// Return it.
+			return component;
+		}
+	}
+
+	// No requested component found.
+	return nullptr;
+}
+
+CComponent* CGameObject::GetComponent(const string& aName)
+{
+	// Search through the components.
+	for (auto component : m_Components)
+	{
+		// If we found the correct one...
+		if (component->GetName() == aName)
+		{
+			// Return it.
+			return component;
+		}
+	}
+
+	// No requested component found.
+	return nullptr;
+}
+
+vector<CComponent*> CGameObject::GetComponents()
+{
+	return m_Components;
+}
+
+vector<CComponent*> CGameObject::GetComponents(const int& aType)
+{
+	// Make the list of components that we've found.
+	vector<CComponent*> components = {};
+
+	// Search through the components.
+	for (auto component : m_Components)
+	{
+		// If we found a matching one...
+		if (component->IsType(aType))
+		{
+			// Add it to the vector.
+			components.push_back(component);
+		}
+	}
+
+	// Return the vector.
+	return components;
+}
+
+vector<CComponent*> CGameObject::GetComponents(const string& aName)
+{
+	// Make the list of components that we've found.
+	vector<CComponent*> components = {};
+
+	// Search through the components.
+	for (auto component : m_Components)
+	{
+		// If we found a matching one...
+		if (component->GetName() == aName)
+		{
+			// Add it to the vector.
+			components.push_back(component);
+		}
+	}
+
+	// Return the vector.
+	return components;
 }
--- Clover Engine/Source/Core/GameObject.h
+++ Clover Engine/Source/Core/GameObject.h
@@ -12,14 +12,13 @@ Website:	http://charlottebrown.ca/
 #ifndef __GAMEOBJECT_H
 #define __GAMEOBJECT_H
 
+#include <string>
 #include <vector>
 
 namespace CloverEngine
 {
 	// Forward declarations.
-	class CComponent;
 	class CTransform;
-	class CDrawBuffer;
 
 	/*
 	Everything that can be added to the game world is a GameObject. GameObjects contain a list of
@@ -42,6 +41,30 @@ namespace CloverEngine
 
 		// Gets this GameObject's transform component.
 		CTransform* GetTransform();
+
+		// Add a component to the GameObject. You can not add another transform.
+		CComponent* AddComponent(CComponent* aComponent);
+
+		/* Gets the first component in the GameObject that are of the requested type. This will
+		return a null pointer if no components match. Use EComponentTypes for built-in engine
+		types, or your own reference if you have defined custom types. */
+		CComponent* GetComponent(const int& aType);
+
+		/* Gets the first component in the GameObject whos name matches the given name. This will
+		return a null pointer if no components match. */
+		CComponent* GetComponent(const std::string& aName);
+
+		// Gets all of the components in this GameObject.
+		std::vector<CComponent*> GetComponents();
+
+		/* Gets all of the components in the GameObject that are of the requested type. This will
+		return an empty vector if no components match. Use EComponentTypes for built-in engine
+		types, or your own reference if you have defined custom types. */
+		std::vector<CComponent*> GetComponents(const int& aType);
+
+		/* Gets all of the components in the GameObject whos names match the given name. This will
+		return an empty vector if no components match. */
+		std::vector<CComponent*> GetComponents(const std::string& aName);
 	};
 }
 
